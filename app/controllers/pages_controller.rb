25f8111081b7d6de34627e1f1e2093a68ae23553
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh[D[D[D[C[C[B <paulosagie15>
2025-12-20 11:24:49 +0000

    lions club

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
   end
+  def contact
+  end
+  def About
+  end
+  def Gallary
+  end
+  def member
+  end
+  def Donation
+  end
+  def join us
+  end
+  def community_work
+  end
+  def investiture
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+get "contact", to: "pages#contact"
+get "About", to: "pages#About"
+get "Gallary", to:"pages#Gallary"
+get "member", to:"pages#member"
+get "Donation", to: "pages#Donation"
+get "impact", to: "pages#impact"
+get "join us", to:"pages#join us"
+get "community_work", to:"pages#community_work"
+get "investiture", to:"pages#investiture"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
