3bef94d0a8f98ef30ec30e37fc73c38769580566
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh[D[D[D[C[C[B <paulosagie15>
2026-01-04 18:57:12 +0000

    elites

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ get "join us", to:"pages#join us"
 get "community_work", to:"pages#community_work"
 get "investiture", to:"pages#investiture"
 get "humanitarian", to:"pages#humanitarian"
+get "disaster", to:"pages#disaster"
+get "enviroment", to:"pages#enviroment"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
